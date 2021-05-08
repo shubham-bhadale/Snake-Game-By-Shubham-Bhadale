31522031dfab5391898530790aba1a0814518150
bhadaleShubham07 <68594212+bhadaleShubham07@users.noreply.github.com>
2021-05-08 09:38:15 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -115,25 +115,25 @@ window.addEventListener('keydown', e =>{
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp");
+            //console.log("ArrowUp");
             inputDir.x = 0;
             inputDir.y = -1;
             break;
 
         case "ArrowDown":
-            console.log("ArrowDown");
+            //console.log("ArrowDown");
             inputDir.x = 0;
             inputDir.y = 1;
             break;
 
         case "ArrowLeft":
-            console.log("ArrowLeft");
+            //console.log("ArrowLeft");
             inputDir.x = -1;
             inputDir.y = 0;
             break;
 
         case "ArrowRight":
-            console.log("ArrowRight");
+            //console.log("ArrowRight");
             inputDir.x = 1;
             inputDir.y = 0;
             break;
