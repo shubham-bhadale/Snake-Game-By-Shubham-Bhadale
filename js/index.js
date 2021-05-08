a18d61f6a087600b8f33af5695608cf9473bba19
Shubham Bhadale <68594212+shubham-bhadale@users.noreply.github.com>
2021-05-08 18:35:45 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,22 +3,24 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 15;
+let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
     {x: 13, y: 15}
 ];
 let isGameRunning = true;
+let controlsInfoPopupOpen = false;
 
 food = {x: 6, y: 7};
 
 function main(ctime) {
-    if(isGameRunning == true)
+	if(isGameRunning == true)
 	{
     	window.requestAnimationFrame(main);
 	}
     // console.log(ctime)
+    
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
@@ -44,11 +46,11 @@ function gameEngine(){
     //Updating the snake array & Food
     if(isCollide(snakeArr)){
         gameOverSound.play();
-        //musicSound.pause();
+        musicSound.pause();
         inputDir =  {x: 0, y: 0}; 
-        alert("Game Over. Press Enter to play again!");
+        alert("Game Over. Press any key to play again!");
         snakeArr = [{x: 13, y: 15}];
-        //musicSound.play();
+        musicSound.play();
         score = 0; 
     }
 
@@ -102,7 +104,7 @@ function gameEngine(){
 
 }
 
-//musicSound.play();
+musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
     hiscoreval = 0;
@@ -151,15 +153,29 @@ window.addEventListener('keydown', e =>{
             break;
 
         case " ":
-            //console.log("SpaceBar");
-            isGameRunning = isGameRunning ? false : true;
-            if(isGameRunning)
-            {
-            	window.requestAnimationFrame(main);
-            }
+        	if(!controlsInfoPopupOpen)
+        	{
+	            //console.log("SpaceBar");
+	            isGameRunning = isGameRunning ? false : true;
+	            if(isGameRunning)
+	            {
+	            	window.requestAnimationFrame(main);
+	            }
+	        }
             break;
         default:
             break;
     }
 
+});
+
+document.getElementById("settingsBox").addEventListener('click', function(e) {
+	isGameRunning = false;
+	controlsInfoPopupOpen = true;
+	document.getElementById('popUpBox').style.display = 'block';
+});
+
+document.getElementById("popUpBox").addEventListener('click', function(e) {
+	controlsInfoPopupOpen = false;
+	document.getElementById('popUpBox').style.display = 'none';
 });
